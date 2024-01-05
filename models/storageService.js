1994add05715f2574bb871e70629aa1f1a603828
AdventisTech <154313626+AdventisTech@users.noreply.github.com>
2024-01-05 16:28:42 +0530

    Add files via upload

---
+++ models/storageService.js
@@ -0,0 +1,52 @@
+// storageService.js
+
+// const fs = require('fs').promises;
+// const path = require('path');
+
+// module.exports = {
+//   saveFile: async (fileData, destination) => {
+//     try {
+//       // Generate a unique filename by appending the current timestamp and the original file extension
+//       const filename = Date.now() + '-' + fileData.originalname;
+
+//       // Convert ArrayBuffer to Buffer
+//       const buffer = Buffer.from(fileData.buffer);
+
+//       // Save the file to the specified destination
+//       const filePath = path.join(destination, filename);
+
+//       await fs.writeFile(filePath, buffer);
+
+//       return filePath;
+//     } catch (error) {
+//       throw error;
+//     }
+//   },
+// };
+
+
+
+const fs = require('fs').promises;
+const path = require('path');
+
+module.exports = {
+  saveFile: async (fileData, destination) => {
+    try {
+      // Generate a unique filename by appending the current timestamp and the original file extension
+      const filename = Date.now() + '-' + fileData.originalname;
+
+      // Convert ArrayBuffer to Buffer
+      const buffer = Buffer.from(fileData.buffer);
+
+      // Save the file to the specified destination
+      const filePath = path.join(destination, filename);
+
+      await fs.writeFile(filePath, buffer);
+
+      return filePath; // Return the file path
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
