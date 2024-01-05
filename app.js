21a2d4a52a2a659fd5d6b6735837318f123a1f62
AdventisTech <154313626+AdventisTech@users.noreply.github.com>
2024-01-05 17:38:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+const multer = require('multer'); // Add this line
+
+const userRoute = require('./route/Registration');
+const projectRoute = require('./route/projects');
+const UserprojectRoute = require('./route/Userprojects');
+
+// Use disk storage for handling file uploads
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/'); // Specify the destination directory
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' + file.originalname);
+    },
+  });
+
+  const upload = multer({ storage: storage });
+
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(parser.json());
+app.use(express.json());
+// app.use(cors());
+// Specify CORS options
+const corsOptions = {
+  origin: 'https://cstimesheet.onrender.com/api/user/Register', // Update this with your frontend's actual URL
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+// Set up Multer middleware for handling file uploads
+app.use(upload.any());
+
+app.use('/api/user', userRoute);
+app.use('/api/user', projectRoute);
+app.use('/api/user', UserprojectRoute);
+
+module.exports = app;
