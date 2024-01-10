cf43efdeb8d60ff84674047fda9b9e38730ed1d4
AdventisTech <154313626+AdventisTech@users.noreply.github.com>
2024-01-10 15:55:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+const env = require('dotenv');
+require('dotenv').config();
+
+env.config();
+
+mongoose.connect(process.env.MONGODB).then(()=>{
+    console.log('connected');
+}).catch(()=>{
+    console.log("failed");
+})
+
+// app.listen(process.env.PORT,()=>{
+//     console.log(`server is running on ${process.env.PORT}`);
+//     console.log(`process.env.MONGODB ${process.env.MONGODB}`)
+// })
+
+// app.listen(port, '0.0.0.0', () => {
+//     console.log(`Server is running on port ${port}`);
+//   });
+
+app.listen(process.env.PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
