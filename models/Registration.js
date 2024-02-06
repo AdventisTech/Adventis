9557901fbc99ac44cca1681a7b388f8932046fc1
AdventisTech <154313626+AdventisTech@users.noreply.github.com>
2024-02-06 09:23:25 +0530

    Add files via upload

---
+++ models/Registration.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const employeeSchema = mongoose.Schema({
+    EmployeeId:{ type: String, unique: true, required: [true, 'EmployeeIdshould be mandatory'] },
+    Name: { type: String, unique: false, required: [true, 'Name should be mandatory'] },
+    Designation: { type: String, unique: false, required: [true, 'Designation should be mandatory'] },
+    Salary: { type: Number, unique: false, required: [false, 'Salary should not be mandatory'] },
+    Emailaddress: { type: String, unique: true, required: [true, 'Email should be mandatory'] },
+    Password: {
+        type: String,
+        unique: false,
+        required: [true, 'Password should be mandatory'],
+      },
+    profilePicture: { type: String, required: [false, 'picture should not be mandatory'] },
+});
+module.exports = mongoose.model('Employe',employeeSchema);
