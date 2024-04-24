78bb49d98a4dc46af930ed87e1f33e47fbdb3150
AdventisTech <154313626+AdventisTech@users.noreply.github.com>
2024-04-24 16:50:38 +0530

    Add files via upload

---
+++ models/projects.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Project = mongoose.Schema({
+    ProjectId:{ type: String, required: true, unique: true},
+    Title: { type: String, required: true },
+    Client: { type: String,  required: true  },
+    ProjectValue:{ type: Number,  required: true  },
+    ResourceBudget:{ type: Number,  required: true  },
+    ProjectDurationFrom:{ type: String,  required: true  },
+    ProjectDurationTo:{ type: String,  required: true  },
+    Description:{ type: String,  required: false },
+    PONumber:{ type: String, required: false},
+    PODate:{ type: String,  required: false  },
+
+});
+Project.index({ ProjectId: 1 }, { unique: true });
+const Project1 = mongoose.model('Project',Project );
+module.exports = Project1;
