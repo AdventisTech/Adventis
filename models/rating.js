14250d57cb3181ba34692b39a463bdbb76b350a9
AdventisTech <154313626+AdventisTech@users.noreply.github.com>
2024-05-02 16:17:03 +0530

    Add files via upload

---
+++ models/rating.js
@@ -0,0 +1,35 @@
+// const mongoose = require('mongoose');
+// const rating = mongoose.Schema({
+//     EmployeeId:{ type: String,  required: [true, 'EmployeeIdshould be mandatory'] },
+//     Name: { type: String,  required: [true, 'Name should be mandatory'] },
+//     selectedDate: { type: String, required: [true, 'Data should be mandatory'] },
+//     Finalrating: { type: String,  required: [true, 'Rating should be mandatory'] },
+// });
+// module.exports = mongoose.model('rating',rating);
+
+const mongoose = require('mongoose');
+
+const ratingSchema = mongoose.Schema({
+    EmployeeId: { type: String, required: true },
+    Name: { type: String, required: true },
+    selectedDate: { type: String, required: true },
+    Finalrating: { type: String, required: true }
+});
+
+// Define compound index for EmployeeId and selectedDate
+ratingSchema.index({ EmployeeId: 1, selectedDate: 1 }, { unique: true });
+
+// Middleware to check if the selectedDate is unique for the given EmployeeId
+ratingSchema.pre('save', async function (next) {
+    try {
+        const existingRating = await this.constructor.findOne({ EmployeeId: this.EmployeeId, selectedDate: this.selectedDate });
+        if (existingRating) {
+            throw new Error('Selected date already exists for this user');
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = mongoose.model('SelfRating', ratingSchema);
